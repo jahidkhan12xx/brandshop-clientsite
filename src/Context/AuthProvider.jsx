f99623be9d0af883d178f996188d24072cfe3e0c
Jahid Kham <jahidkhan12xx@gmail.com>
2023-10-21 14:55:17 +0600

    user update profile

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -17,9 +17,10 @@ const AuthProvider = ({children}) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
-    const updateUser = (name) =>{
+    const updateUser = (name,photo) =>{
         return updateProfile(auth.currentUser,{
-            displayName : name
+            displayName : name,
+            photoURL : photo,
         })
     }
 
